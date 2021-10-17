55b721a36e911b45898f066e2c7ddbef5f9b4be2
Alister <hi@alisternorris.com>
2021-10-17 15:24:51 +0100

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,4 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
-function render() {
-  ReactDOM.render(<h2>Hello from React!</h2>, document.getElementById('root'));
-}
-
-render();
+ReactDOM.render(<h2>Hello from test!</h2>, document.getElementById('root'));
